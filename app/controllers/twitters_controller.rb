a781d99b9cd4daee938ef9a62311d589d41224e4
ukayare <ukayare@earth.ocn.ne.jp>
2011-08-24 17:07:57 +0900

    twitter

---
+++ app/controllers/twitters_controller.rb
@@ -0,0 +1,14 @@
+﻿require 'twitter'
+require 'pp'
+class TwitterController < ActionController::Base
+
+  def test
+    Twitter.configure do |config|
+      config.consumer_key = 'RZb8v1lbXJAzeBSA6KWQ'
+      config.consumer_secret = 'Mc6NEpxhhsUJpUTmKCh7Gt8o0DcbwNLNBnw3WoVq2SQ'
+      config.oauth_token = '96814807-u3S4EgNlURQrUt8o2kjYQtTKp2SqzXgIa0zuszqgv'
+      config.oauth_token_secret = 'MJhdy72I2qj7gOErc5lXhaGfP6uuZ70uPConlGcM'
+    end
+    line=Twitter.user_timeline("ainame")
+  end
+end
