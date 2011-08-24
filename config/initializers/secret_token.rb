a6804fac1f75509650f8254b49125552f77be04e
ainame <ainame954@facebook.com>
2011-08-24 13:05:00 +0900

    add initialized rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestIdiary::Application.config.secret_token = 'e055ef31e0de6051aed43674f460567b22f67856d354725dbe48c7c7c0369b68c84982edec71ddfa971cfbeac0a13a82c56c942ad5fa3182d9500ea135bde34e'
