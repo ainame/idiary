81b2cfa8b655c67f9d267363dde072bf35f62e32
ukayare <ukayare@earth.ocn.ne.jp>
2011-08-24 15:47:32 +0900

    twitter名前と発言

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -4,12 +4,12 @@ require 'pp'
 class TwitterController < ActionController::Base
 
   define test
-     # your app
-      oauth = Twitter::OAuth.new('RZb8v1lbXJAzeBSA6KWQ', 'Mc6NEpxhhsUJpUTmKCh7Gt8o0DcbwNLNBnw3WoVq2SQ')
-      # your login
-      oauth.authorize_from_access('96814807-u3S4EgNlURQrUt8o2kjYQtTKp2SqzXgIa0zuszqgv', 'MJhdy72I2qj7gOErc5lXhaGfP6uuZ70uPConlGcM')
-      twitter = Twitter::Base.new(oauth)
-      
-      pp twitter.user_timeline
+    Twitter.configure do |config|
+      config.consumer_key = 'RZb8v1lbXJAzeBSA6KWQ'
+      config.consumer_secret = 'Mc6NEpxhhsUJpUTmKCh7Gt8o0DcbwNLNBnw3WoVq2SQ'
+      config.oauth_token = '96814807-u3S4EgNlURQrUt8o2kjYQtTKp2SqzXgIa0zuszqgv'
+      config.oauth_token_secret = 'MJhdy72I2qj7gOErc5lXhaGfP6uuZ70uPConlGcM'
+    end
+    line=Twitter.home_timeline
   end
 end
